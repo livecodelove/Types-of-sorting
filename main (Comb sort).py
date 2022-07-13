8fd8e6016c4db36b532ca1e544bf43a1b27ad4ca
Elizabeth Milichenkova <108712381+livecodelove@users.noreply.github.com>
2022-07-13 10:50:07 +0700

    Comb sort
    Add file with "Comb sort"

---
+++ main (Comb sort).py
@@ -0,0 +1,20 @@
+arr= [3, 14, 1, 7, 9, 8, 11, 6, 4, 2]
+
+
+choice_arr = input('Hello, choose an array option, t - test array, n - new (yours array) \n')
+if choice_arr == 'n':
+    arr =  list(map (int, input().split()))
+    
+l = len(arr)
+factor = 1.247
+step_factor = l / factor 
+
+
+while step_factor > 1:
+    step = round(step_factor)
+    for j in range(step, l):
+        for i in range(j):
+          if arr[i] > arr[j]:
+              arr[i], arr[j] = arr[j], arr[i]
+    step_factor = step_factor/factor
+print(arr)
